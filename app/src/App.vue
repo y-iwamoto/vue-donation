ffdefa6a93f0bbfd2bba69e0f58fab7ee7efb596
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-08 23:28:22 +0900

    blumaを導入

--- app/src/App.vue
+++ app/src/App.vue
@@ -12,7 +12,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
