ea6487548bcb71290d76b03e3cea0832aeced654
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-10 21:28:48 +0900

    ユーザ情報表示のみのダッシュボード画面を作成

---
+++ app/src/components/pages/DashBoard.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="columns">
+    <div class="column"></div>
+    <div class="column column is-two-fifths">
+        <DashBoardHeader
+            :username="getUser.username"
+            :wallet="getUser.wallet"
+        />
+        <Title :title="title" />
+        <div class="mt-3">
+            <h2 class="is-size-5 has-text-weight-bold">ユーザ名</h2>
+        </div>
+    </div>
+    <div class="column"></div>
+</div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import Title from '../parts/Title.vue'
+import DashBoardHeader from '../parts/DashBoardHeader.vue'
+export default {
+    name: "DashBoard",
+    components: {
+        Title,
+        DashBoardHeader
+    },
+    data() {
+        return {
+            title: "ユーザ一覧",
+        }
+    },
+    computed: {...mapGetters("users",["getUser"])}
+}
+</script>
