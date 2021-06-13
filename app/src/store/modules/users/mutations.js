8d379954d1f105edc7072f7373273176b4e58123
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-13 20:53:43 +0900

    一部usersのvuexのメソッド名を変えつつDashBoard.vue部分を要件に合わせて修正

--- app/src/store/modules/users/mutations.js
+++ app/src/store/modules/users/mutations.js
@@ -1,16 +1,16 @@
 const mutations = {
-  setUserInfo(state, payload) {
-    const { email, uid } = payload;
-    state.email = email;
-    state.uid = uid;
-  },
-  setUserNameAndWallet(state, payload) {
-    const { username, wallet } = payload;
-    state.username = username;
-    state.wallet = wallet;
-  },
-  setError(state, payload) {
-    state.error = payload;
-  }
-};
-export default mutations;
+    setMyWalletInfo(state, payload) {
+      const { email, uid } = payload;
+      state.email = email;
+      state.uid = uid;
+    },
+    setUserNameAndWallet(state, payload) {
+        const { username, wallet } = payload;
+        state.username = username;
+        state.wallet = wallet
+    },
+    setError(state, payload) {
+      state.error = payload;
+    }
+  };
+  export default mutations;
