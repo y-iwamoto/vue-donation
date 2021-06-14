c3eeaafe1eba3e3ff1add14ba871334278420b06
yuki-iwamoto <yuki_iwamoto@genesis-healthcare.jp>
2021-06-14 23:19:39 +0900

    投げ銭処理部分実装

--- app/src/store/modules/wallets/actions.js
+++ app/src/store/modules/wallets/actions.js
@@ -15,6 +15,45 @@ const actions = {
           .filter(wallet => wallet.uid !== myUid);
         commit('setWalletList', { wallets });
       });
+  },
+  async requestDonation({ dispatch, commit }, payload) {
+    const targetWallet = await dispatch(
+      'fetchLatestWallet',
+      payload.targetWallet
+    );
+    await dispatch('adjustMoney', targetWallet);
+
+    const myWallet = await dispatch('fetchLatestWallet', payload.myWallet);
+    await dispatch('adjustMoney', myWallet);
+    commit(
+      'users/setUserNameAndWallet',
+      {
+        username: store.getters['users/getMyWalletInfo'].username,
+        wallet: myWallet.wallet
+      },
+      { root: true }
+    );
+    dispatch('fetchWalletList');
+  },
+  async adjustMoney(_, payload) {
+    let updateData = {};
+    updateData['/donation/' + payload.uid + '/wallet'] = payload.wallet;
+    return await firebase
+      .database()
+      .ref()
+      .update(updateData);
+  },
+  async fetchLatestWallet(_, payload) {
+    const userInfo = await firebase
+      .database()
+      .ref()
+      .child('donation')
+      .child(payload.uid)
+      .get();
+    return {
+      wallet: Number(userInfo.val().wallet) + Number(payload.donation),
+      uid: payload.uid
+    };
   }
 };
 export default actions;
